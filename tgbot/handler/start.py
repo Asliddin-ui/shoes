19dc6f29804b2a6cab84077c2bcadd537734cb9e
Asliddin <asliddinjaloliddinov12@gmail.com>
2023-08-16 10:40:52 +0500

    Bot add

--- tgbot/handler/start.py
+++ tgbot/handler/start.py
@@ -12,12 +12,15 @@ async def user_load(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     try:
         tg_user = await TelegramUser.objects.aget(telegram_user_id=user_id)
-        await hello(update, context, True)
+        print('Function ishlavoti')
+        await hello(update, context, edit=True)
     except TelegramUser.DoesNotExist:
+        print('bu ham ishladi')
         tg_user = await TelegramUser.objects.acreate(
-            update.effective_user.username,
-            update.effective_user.first_name,
-            update.effective_user.last_name
+            telegram_user_id=user_id,
+            username=update.effective_user.username,
+            first_name=update.effective_user.first_name,
+            last_name=update.effective_user.last_name
         )
 
     context.tguser = tg_user
@@ -26,6 +29,7 @@ async def user_load(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 
 async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE, edit=False) -> None:
+    user_id = update.effective_user.id
     params = {
         'text': _('Xush kelibsiz {}. Botdan foydalanish uchun tilni tanlang: ').format(
             update.effective_user.first_name),
@@ -36,14 +40,27 @@ async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE, edit=False) 
             update.effective_user.first_name),
         'reply_markup': await get_key()
     }
-
+    try:
+        tg_user = await TelegramUser.objects.aget(telegram_user_id=user_id)
+        edit = True
+        context.tguser = tg_user
+        print('Bu ishladi')
+    except TelegramUser.DoesNotExist:
+        print('bu ham ishladi')
+        tg_user = await TelegramUser.objects.acreate(
+            telegram_user_id=user_id,
+            username=update.effective_user.username,
+            first_name=update.effective_user.first_name,
+            last_name=update.effective_user.last_name
+        )
+        context.tguser = tg_user
     if edit:
         try:
-            print("A")
+            print("B")
             await update.effective_message.edit_text(**params2)
         except:
-            print('B')
-            # await update.effective_message.delete()
+            print('A')
+            await update.effective_message.delete()
             await update.effective_message.reply_text(**params2)
     else:
         print('C')
@@ -61,7 +78,7 @@ async def change_language(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 
 handlers = [
-    (CustomHandler(user_load), 0),
+    # (CustomHandler(user_load), 0),
     CommandHandler('start', hello),
     CallbackQueryHandler(change_language, pattern="^(uz|ru|en)$"),
 ]
